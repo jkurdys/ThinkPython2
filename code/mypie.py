d50ac515f99c2391783828070b1179449916fa25
Joshua Kurdys <jkurdys@gmail.com>
2021-11-19 10:19:58 -0700

    updated mypie.py

--- code/mypie.py
+++ code/mypie.py
@@ -2,19 +2,41 @@ import turtle
 import math
 import numpy as np
 
-def pie(s, l):
+def pie(t, s, l):
     a = 360/s
     for i in range(s):
+        tri(t, l, a/2)
         t.lt(a)
-        tri(l, a/2)
 
-def tri(l, a):
-    for i in range(3):
-        t.fd(l)
-        t.lt(90 + a)
-        t.fd()
+def tri(t, l, a):
+    y = l * math.sin(a * math.pi / 180)
+    
+    t.rt(a)
+    t.fd(l)
+    t.lt(90 + a)
+    t.fd(2 * y)
+    t.lt(90 + a)
+    t.fd(l)
+    t.lt(180 - a)
+
+def draw_pies(t, ps, ss, ls):
+    t.pu()
+    for i in range(ps):
+        if i == 0:
+            t.fd(-(2*ls[i] + 10))
+            t.pd()
+            pie(t, ss[i], ls[i])
+            t.pu()
+        else:
+            t.fd(2*ls[i] + 10)
+            t.pd()
+            pie(t, ss[i], ls[i])
+            t.pu()
 
 if __name__ == '__main__':
     t = turtle.Turtle()
-    pie(4, 100)
+    ps = 3
+    ss = [5, 6, 7]
+    ls = [40, 40, 40]
+    draw_pies(t, ps, ss, ls)
     turtle.mainloop()
